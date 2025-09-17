24c6b4505271cbefb3367d026fc0b9e4f1c2a588
cvfisher <cvfisher.93@gmail.com>
2025-09-17 20:06:02 +0100

    remove node modules

--- js/postcss.config.js
+++
@@ -1,8 +0,0 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-	content: ["./**/*.html"],
-	defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
-module.exports = {
-	plugins: [require("autoprefixer"), purgecss],
-};
